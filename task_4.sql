7402eb912fb29ff8285d921599c9875c1fe57992
Gutu <gutujirex@gmail.com>
2025-08-17 14:22:10 +0300

    task4 corrected

--- task_4.sql
+++ task_4.sql
@@ -1,13 +1,5 @@
--- Prints the full description of the books table
-SELECT 
-    COLUMN_NAME, 
-    COLUMN_TYPE, 
-    IS_NULLABLE, 
-    COLUMN_KEY, 
-    COLUMN_DEFAULT, 
-    EXTRA 
-FROM 
-    INFORMATION_SCHEMA.COLUMNS 
-WHERE 
-    TABLE_SCHEMA = 'alx_book_store' 
-    AND TABLE_NAME = 'books';
+-- Full description of books table without DESCRIBE or EXPLAIN
+SELECT * 
+FROM INFORMATION_SCHEMA.COLUMNS 
+WHERE TABLE_SCHEMA = 'alx_book_store' 
+AND TABLE_NAME = 'books';
